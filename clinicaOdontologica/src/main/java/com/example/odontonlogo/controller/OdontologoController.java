485b90a2cc243f2565c2032727189f7d88942d22
Sarahalfonsin <sarahalfonsin@hotmail.com>
2022-04-19 23:19:56 -0300

    cargas

--- clinicaOdontologica/src/main/java/com/example/odontonlogo/controller/OdontologoController.java
+++ clinicaOdontologica/src/main/java/com/example/odontonlogo/controller/OdontologoController.java
@@ -21,7 +21,7 @@ public class OdontologoController {
 
     //que implemente el service
 @Autowired
-    IOdontologoService odontologoService;
+    OdontologoService odontologoService;
 
 //guardar
 @PostMapping()
